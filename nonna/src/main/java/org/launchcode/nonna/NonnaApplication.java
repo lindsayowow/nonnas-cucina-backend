8c30cdb35537e9ab3db190037892185387beea5c
lindsayowow <lindsayow@gmail.com>
2026-08-19 20:59:18 -0500

    First Commit - boilerplate project structure

---
+++ nonna/src/main/java/org/launchcode/nonna/NonnaApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.nonna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonnaApplication.class, args);
+	}
+
+}
