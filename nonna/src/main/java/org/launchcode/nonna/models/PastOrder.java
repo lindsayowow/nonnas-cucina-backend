e3060a21c2281f8479781e33cadbbd917a2c237d
lindsayowow <lindsayow@gmail.com>
2026-08-29 23:19:57 -0500

    Added PastOrder model

--- nonna/src/main/java/org/launchcode/nonna/models/PastOrder.java
+++ nonna/src/main/java/org/launchcode/nonna/models/PastOrder.java
@@ -1,4 +1,27 @@
 package org.launchcode.nonna.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "past_orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PastOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private Timestamp orderTimeStamp;
+    private double orderTotal;
+
+//    @ManyToOne
+//    @JoinColumn(name = "user_id")
+//    private User user;
+
 }
