5d67db20f9739da7cd16be0f51b79049031e6672
lindsayowow <lindsayow@gmail.com>
2026-08-31 23:15:39 -0500

    added UserDTO

---
+++ nonna/src/main/java/org/launchcode/nonna/dtos/UserDTO.java
@@ -0,0 +1,100 @@
+package org.launchcode.nonna.dtos;
+
+import org.launchcode.nonna.models.User;
+
+public class UserDTO {
+
+    private int id;
+    private String username;
+    private String passwordHash;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String role;
+    private String streetAddress;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String phoneNumber;
+
+    public UserDTO(){}
+
+    public UserDTO(int id, String username, String passwordHash, String email, String firstName, String lastName, String role, String streetAddress, String city, String state, String zipCode, String phoneNumber) {
+        this.id = id;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public UserDTO(User user){
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.passwordHash = user.getPasswordHash();
+        this.email = user.getEmail();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.role = user.getRole();
+        this.streetAddress = user.getStreetAddress();
+        this.city = user.getCity();
+        this.state = user.getState();
+        this.zipCode = user.getZipCode();
+        this.phoneNumber = user.getPhoneNumber();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+}
