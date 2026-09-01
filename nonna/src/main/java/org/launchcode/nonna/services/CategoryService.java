05e71c46abf19d94ed8606015475a095471f0b88
lindsayowow <lindsayow@gmail.com>
2026-08-31 23:35:46 -0500

    updated CategoryService to connect to DTO

--- nonna/src/main/java/org/launchcode/nonna/services/CategoryService.java
+++ nonna/src/main/java/org/launchcode/nonna/services/CategoryService.java
@@ -1,5 +1,6 @@
 package org.launchcode.nonna.services;
 
+import org.launchcode.nonna.dtos.CategoryDTO;
 import org.launchcode.nonna.models.Category;
 import org.launchcode.nonna.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
@@ -10,18 +11,33 @@ import java.util.List;
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
-    public CategoryService(CategoryRepository categoryRepository)
-    {this.categoryRepository=categoryRepository;}
 
-    public List<Category> getAllCategories()
-    {return categoryRepository.findAll();}
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
-    public Category getByCategoryId(int id)
-    {return categoryRepository.findById(id).orElse(null);}
+    public List<CategoryDTO> getAllCategoryDTOs() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream()
+                .map(CategoryDTO::new)
+                .toList();
+    }
 
-    public Category saveCategory(Category category)
-    {return categoryRepository.save(category);}
+    public CategoryDTO getByCategoryDTOId(int id) {
+        return categoryRepository.findById(id)
+                .map(CategoryDTO::new)
+                .orElse(null);
+    }
 
-    public void deleteCategory(int id)
-    {categoryRepository.deleteById(id);}
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void deleteCategory(int id) {
+        categoryRepository.deleteById(id);
+    }
+
+    private CategoryDTO convertToDTO(Category category) {
+        return new CategoryDTO(category);
+    }
 }
