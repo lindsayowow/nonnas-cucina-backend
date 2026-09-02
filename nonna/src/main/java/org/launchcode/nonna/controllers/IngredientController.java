bcd4f60c509f216499276b9b8ca7ebd4df917a10
lindsayowow <lindsayow@gmail.com>
2026-09-02 18:53:33 -0500

    added Ingredient Controller

---
+++ nonna/src/main/java/org/launchcode/nonna/controllers/IngredientController.java
@@ -0,0 +1,39 @@
+package org.launchcode.nonna.controllers;
+
+import org.launchcode.nonna.dtos.IngredientDTO;
+import org.launchcode.nonna.models.Ingredient;
+import org.launchcode.nonna.services.IngredientService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ingredients")
+public class IngredientController {
+
+    private final IngredientService ingredientService;
+
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    @GetMapping
+    public List<IngredientDTO> getAllIngredients() {
+        return ingredientService.getAllIngredientDTOs();
+    }
+
+    @GetMapping("/{id}")
+    public IngredientDTO getByIngredientDTOId(@PathVariable int id) {
+        return ingredientService.getByIngredientDTOId(id);
+    }
+
+    @PostMapping
+    public Ingredient createIngredient(@RequestBody Ingredient ingredient) {
+        return ingredientService.saveIngredient(ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteIngredient(@PathVariable int id) {
+        ingredientService.deleteIngredient(id);
+    }
+}
