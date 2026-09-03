f3266795ab6c7338d23ed9a82a65ccb7399a3f88
lindsayowow <lindsayow@gmail.com>
2026-09-02 19:05:40 -0500

    updated Category Controller

--- nonna/src/main/java/org/launchcode/nonna/controllers/CategoryController.java
+++ nonna/src/main/java/org/launchcode/nonna/controllers/CategoryController.java
@@ -3,7 +3,6 @@ package org.launchcode.nonna.controllers;
 import org.launchcode.nonna.dtos.CategoryDTO;
 import org.launchcode.nonna.models.Category;
 import org.launchcode.nonna.services.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,7 +22,7 @@ public class CategoryController {
         return categoryService.getAllCategoryDTOs();
     }
 
-    @GetMapping("/id")
+    @GetMapping("{/id}")
     public CategoryDTO getByCategoryId(@PathVariable int id) {
         return categoryService.getByCategoryDTOId(id);
     }
@@ -33,7 +32,7 @@ public class CategoryController {
         return  categoryService.saveCategory(category);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public void deleteCategory(@PathVariable int id) {
         categoryService.deleteCategory(id);
     }
