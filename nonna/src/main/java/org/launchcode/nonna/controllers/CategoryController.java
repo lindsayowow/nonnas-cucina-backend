cd30a9df8145c0d15bc9bdc6d7afd20bd4ac44e8
lindsayowow <lindsayow@gmail.com>
2026-09-02 19:03:54 -0500

    added Category Controller

---
+++ nonna/src/main/java/org/launchcode/nonna/controllers/CategoryController.java
@@ -0,0 +1,40 @@
+package org.launchcode.nonna.controllers;
+
+import org.launchcode.nonna.dtos.CategoryDTO;
+import org.launchcode.nonna.models.Category;
+import org.launchcode.nonna.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<CategoryDTO> getAllCategories() {
+        return categoryService.getAllCategoryDTOs();
+    }
+
+    @GetMapping("/id")
+    public CategoryDTO getByCategoryId(@PathVariable int id) {
+        return categoryService.getByCategoryDTOId(id);
+    }
+
+    @PostMapping
+    public Category createCategory(@RequestBody Category category) {
+        return  categoryService.saveCategory(category);
+    }
+
+    @DeleteMapping
+    public void deleteCategory(@PathVariable int id) {
+        categoryService.deleteCategory(id);
+    }
+}
