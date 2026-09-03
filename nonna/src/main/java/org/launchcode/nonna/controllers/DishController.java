bc2d2115cba9497150fc51e5ce689d17a286eb45
lindsayowow <lindsayow@gmail.com>
2026-09-02 19:47:51 -0500

    updated Category - working on connecting through Postman, changed int to Integer

---
+++ nonna/src/main/java/org/launchcode/nonna/controllers/DishController.java
@@ -0,0 +1,4 @@
+package org.launchcode.nonna.controllers;
+
+public class DishController {
+}
--- nonna/src/main/java/org/launchcode/nonna/models/Category.java
+++ nonna/src/main/java/org/launchcode/nonna/models/Category.java
@@ -14,7 +14,7 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private Integer id;
 
     private String categoryName;
 
