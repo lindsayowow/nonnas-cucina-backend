b7e69d4cd4607aed9184357c638535ec027df02b
lindsayowow <lindsayow@gmail.com>
2026-09-07 11:53:02 -0500

    added UpdateProfileDTO

---
+++ nonna/src/main/java/org/launchcode/nonna/dtos/UpdateProfileDTO.java
@@ -0,0 +1,8 @@
+package org.launchcode.nonna.dtos;
+
+public class UpdateProfileDTO {
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+}
